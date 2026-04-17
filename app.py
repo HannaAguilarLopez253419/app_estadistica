d794f2469b3bdb4ee8a28a8a8d0cfd6fd4537f9c
HannaAguilarLopez253419 <you@example.comgit config --global user.email you@example.com>
2026-04-16 19:52:27 -0600

    Implementacion carga de archivos CVS

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+import pandas as pd
 import streamlit as st
 
 st.title("App de Estadística distribuciones de probabilidad y prueba de hipotesis")
-
 st.write("Bienvenido a la aplicación")
+st.header("Carga de datos")
+archivo = st.file_uploader("Sube un archivo CSV", type=["csv"])
+if archivo:
+    df = pd.read_csv(archivo)
+    st.write("Vista previa de los datos:")
+    st.write(df.head())
